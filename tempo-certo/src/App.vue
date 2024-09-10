bda57f3ed578ba28444a91e732de38ec3ad8c824
motta-phenrique <motta.phenrique@gmail.com>
2024-09-09 21:15:26 -0300

    firstcommit

---
+++ tempo-certo/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import NavBar from "./components/comuns/NavBar.vue";
+</script>
+
+<template>
+  <div>
+    <NavBar />
+    <RouterView />
+
+    <div class="h-3">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tempo-certo/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>aaaaaaa</h1>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup>
+
+</script>
